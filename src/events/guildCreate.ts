625d4e13fade767083431ca28c2cc08a2a65ba29
Andrey Grunev <EternallLight@users.noreply.github.com>
2020-05-15 11:58:11 +0300

    Initial commit

---
+++ src/events/guildCreate.ts
@@ -0,0 +1,5 @@
+import { Guild } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/v1/structures/guild.ts";
+
+export const guildCreate = (guild: Guild) => {
+  console.log(guild.name);
+};
